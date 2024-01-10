f9d0bbcb7be0103b7ef2938d8b6769fd5d9efb90
Brian Grouzos <bgrouzos@yahoo.com>
2024-01-09 21:46:01 -0600

    Clean up more spaces

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -150,7 +150,6 @@ def trip_duration_stats(df):
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
 
-
     # display total travel time
     tt = df['Trip Duration']
 
@@ -162,7 +161,6 @@ def trip_duration_stats(df):
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
-
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
